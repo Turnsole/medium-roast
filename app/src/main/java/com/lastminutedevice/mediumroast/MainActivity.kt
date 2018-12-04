ae6385be1291ddee6aa4e183b8f1d1a52e09806a
Christa Mabee <cmabee@zillowgroup.com>
2018-12-04 10:35:23 -0800

    Added button and counter.

--- app/src/main/java/com/lastminutedevice/mediumroast/MainActivity.kt
+++ app/src/main/java/com/lastminutedevice/mediumroast/MainActivity.kt
@@ -2,11 +2,24 @@ package com.lastminutedevice.mediumroast
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.button).setOnClickListener {
+            incrementCounter()
+        }
+    }
+
+    private fun incrementCounter() {
+        counter += 1
+        findViewById<TextView>(R.id.counter).text = counter.toString()
     }
 }
